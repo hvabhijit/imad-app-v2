f20b5fe11676e502a7974772a86715a6cac200a4
Abhijit H V <hvabhijit1990@gmail.com>
2017-02-07 00:52:13 +0530

    serving pages dynamically based on params

--- server.js
+++ server.js
@@ -5,8 +5,72 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+	'article-one': {
+		title: "Article One",
+		heading: "Article One",
+		content: `Today I am working with nodejs and serving this html page.
+			Article One description goes here`
+	},
+
+	'article-two': {
+		title: "Article Two",
+		heading: "Article Two",
+		content: `Today I am working with nodejs and serving this html page.
+			Article Two description goes here`
+	},
+
+	'article-three': {
+		title: "Article Three",
+		heading: "Article Three",
+		content: `Today I am working with nodejs and serving this html page.
+			Article Three description goes here`
+	}
+
+};
+
+function createTemplate(data) {
+	var title = data.title
+	var heading = data.heading
+	var content = data.content
+
+	var htmlTemplate =
+		`<html>
+		<head>
+			<title> ${title} </title>
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<link href="/ui/style.css" rel="stylesheet" />
+		 </head>
+		<body>
+			<div class="container center">
+				<div style="font-size:25px;">
+					Introduction Mobile Application Developement
+					<br>
+					<br>
+		            <img src="/ui/madi.png" class="img-article"/>
+		        </div>
+				<div style="padding-bottom: 10px";> 
+					<a href="/"> Home </a>
+				</div>
+				<div>
+					<strong>${heading}</strong> 
+					<p>
+						${content}
+					</p>
+				</div>
+			</div>
+		</body>
+		</html>`;
+	return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/:articleName', function (req, res) {
+	var articleName = req.params.articleName
+  	res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
